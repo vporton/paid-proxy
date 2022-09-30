088cbb834ae62457ab4f7b56ecb7d17988442d7d
Victor Porton <porton.victor@gmail.com>
2022-09-30 20:26:59 +0300

    proxy works

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import json
+import re
 import struct
 
 import lmdb
+import requests as requests
 from flask_lambda import FlaskLambda
 # from flask import Flask
 from flask import request
@@ -18,44 +20,56 @@ accounts_db = env.open_db(b'accounts', create=True)
 
 # Following https://gist.github.com/questjay/3f858c2fea1731d29ea20cd5cb444e30#file-flask-server-proxy
 def serve_proxied(upstream_path):
-    r = make_request(config['upstreamPrefix'] + upstream_path, request.method, dict(request.headers), request.form)
-    print("RR", r)
-    headers = dict(r.raw.headers)
-    filter_headers(headers)
+    request_headers = dict(request.headers)
+    filter_request_headers(request_headers)
+    r = make_request(config['upstreamPrefix'] + upstream_path + f"&key={config['upstreamKey']}", request.method, headers=request_headers,
+                     data=request.get_data())
+    response_headers = dict(r.raw.headers)
+    filter_response_headers(response_headers)
 
     def generate():
-        for chunk in r.raw.stream(decode_content=False):
+        # for chunk in r.raw.stream(decode_content=True):
+        for chunk in r.iter_content(chunk_size=1024):
             yield chunk
 
-    out = app.response_class(generate(), headers=headers)
+    out = app.response_class(generate(), headers=response_headers)
     out.status_code = r.status_code
     return out  # (r.text, r.status_code, headers)
 
 
-def filter_headers(headers):
-    # http://tools.ietf.org/html/rfc2616#section-13.5.1
-    hop_by_hop = ('Connection', 'Keep-Alive', 'Te', 'Trailers', 'Transfer-Encoding', 'Upgrade')
-    for k in hop_by_hop:
-        if k in headers:
-            del headers[k]
-
-    # accept only supported encodings
-    if 'Accept-Encoding' in headers:
-        ae = headers['Accept-Encoding']
-        filtered_encodings = [x for x in re.split(r',\s*', ae) if x in ('identity', 'gzip', 'x-gzip', 'deflate')]
-        headers['Accept-Encoding'] = ', '.join(filtered_encodings)
-
-    del headers['x-account-id']
+def filter_request_headers(headers):
+    entries_to_remove = [k for k in headers.keys() if k.lower() in ['host', 'x-account-id']]
+    for k in entries_to_remove:
+        del headers[k]
     for k, v in config['upstreamHeaders'].items():
         headers[k] = v
+
+
+def filter_response_headers(headers):
+    # http://tools.ietf.org/html/rfc2616#section-13.5.1
+    hop_by_hop = ['connection', 'keep-alive', 'te', 'trailers', 'transfer-encoding', 'upgrade',
+                  'content-length']  # my addition - Victor Porton
+    entries_to_remove = [k for k in headers.keys() if k.lower() in hop_by_hop]
+    for k in entries_to_remove:
+        del headers[k]
+
+    # FIXME
+    # accept only supported encodings
+    # if 'Accept-Encoding' in headers:
+    #     ae = headers['Accept-Encoding']
+    #     filtered_encodings = [x for x in re.split(r',\s*', ae) if x in ('identity', 'gzip', 'x-gzip', 'deflate')]
+    #     headers['Accept-Encoding'] = ', '.join(filtered_encodings)
 
     return headers
 
 
-def make_request(url, method, headers={}, data=None, requests=None):
+def make_request(url, method, headers={}, data=None):
     try:
         # LOG.debug("Sending %s %s with headers: %s and data %s", method, url, headers, data)
-        return requests.request(method, url, params=request.args, stream=True, headers=headers, allow_redirects=False,
+        print(url)
+        return requests.request(method, url, stream=True,
+                                headers=headers,
+                                allow_redirects=False,
                                 data=data)
     except Exception as e:
         print(e)
@@ -63,10 +77,9 @@ def make_request(url, method, headers={}, data=None, requests=None):
 
 @app.route('/proxy/<path:p>', methods=['GET', 'POST'])
 def proxy_handler(p):
-    account = request.headers['x-account-id']  # TODO: If the header is missing
+    account = {k.lower(): v for k, v in request.headers.items()}['x-account-id']  # TODO: If the header is missing
     account = account.encode('utf-8')  # hack
     for k, v in config['costs'].items():
-        print(f"{p}.startswith({k})")
         if p.startswith(k):
             with env.begin(write=True) as txn:
                 # remainder = txn.get(account)
@@ -77,7 +90,8 @@ def proxy_handler(p):
                 remainder = 100000.0  # FIXME
                 if v <= remainder:
                     txn.put(account, struct.pack('f', remainder - v))
-                    return serve_proxied(p)
+                    upstream_path = re.sub(r"^/proxy/", "", request.full_path)
+                    return serve_proxied(upstream_path)
             break
     return "Path not found."
 
